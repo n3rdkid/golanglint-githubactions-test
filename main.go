ba73b37f10213ee94d81223fa9af96a429d04014
Saurav <sauravads123@gmail.com>
2022-07-05 12:16:53 +0545

    test nolint

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func Test() {
 }
 func main() {
 	log.Println("HELLO")
-	var password = "aftasvqweqweqwd"
-	var abc = 2
+	// var password = "aftasvqweqweqwd" //nolint
+	var abc = 2 //nolint
 	Test()
 }
